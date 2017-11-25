8fadefa683e86918cb890758faf153af3e0a47ce
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2017-11-26 00:14:06 +0100

    man kan nu ta bort poster. lagt till svarsruta.

--- admin/cart_all_list.php
+++ admin/cart_all_list.php
@@ -2,23 +2,32 @@
 <html>
 <head>
   <?PHP include_once "header.php"; ?>
+  <?php
+
+  if (isset($_POST['Delete']))
+  {
+    $cart_id= $_POST['cart_id'];
+    $sql_delete = $conn->query("DELETE FROM cart WHERE cart_id=".$cart_id);
+  }
+
+   ?>
 </head>
 <body>
   <?PHP include_once "admin_navbar.php"; ?>
-  <div class="outer_container pt-4 px-5"><h1 class="text-center">Varukorgen</h1></div>
+  <div class="outer_container pt-4 px-5"><h1 class="text-center">Varukorgen</h1>
+<div id='response' class='mb-3'></div>
+  </div>
   <?PHP
       $sql = "SELECT * FROM cart ORDER BY cart_id DESC";
              $tabell = mysqli_query($conn, $sql)
              or die (mysqli_error($conn));
              echo "<table class='table table-hover table-striped'>";
              echo "<tr>
-                    <th>Ordernummer</th>
+                    <th>Beställningsnummer</th>
                     <th>Kundnummer</th>
                     <th>Artikelnummer</th>
                     <th>Antal</th>
                     <th>Datum</th>
-                    <th>Hash (endast för test)</th>
-                    <th>Markulerad</th>
                     <th>Ändra</th>
                  </tr>";
              while($rad = $tabell ->fetch_assoc()){
@@ -27,15 +36,33 @@
                          <td>".$rad['cart_customer_id']."</td>
                          <td>".$rad['cart_product_id']."</td>
                          <td>".$rad['cart_product_quantity']."</td>
-                         <td>".$rad['cart_date']."</td>
-                         <td>".$rad['cart_session_hash']."</td>
-                         <td>".$rad['cart_cancel']."</td>
-                         <td><a href='customer_card.php?customer_id=".$rad['customer_id']."' class='btn btn-outline-danger'>Markulera</td>
-                     </tr>";
+                         <td>".$rad['cart_date']."</td>"; ?>
+
+                         <td>
+                         <form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+                           <input type='hidden' id='cart_id' name='cart_id' value="<?php echo $rad['cart_id']; ?>">
+                           <button type='submit' Name = 'Delete'  class='btn btn-danger '>Ta bort</button>
+                         </form>
+                         </td>
+
+              <?PHP echo "</tr>";
              }
              echo "</table>";
   ?>
 
 <?PHP include_once "footer.php"; ?>
+
+<?php
+if (isset($_POST['Delete']))
+{
+  echo "<script>
+    $(function (){
+      $('#response').html(\"<div id='response-add' class='bg-warning text-white response_hide text-center p-2'>Artikeln är borttagen.</div>\");
+    });
+  </script>";
+}
+
+ ?>
+
 </body>
 </html>
